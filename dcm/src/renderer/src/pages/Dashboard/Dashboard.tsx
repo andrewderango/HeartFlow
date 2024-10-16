d65c7e0f7ff0962cf2c133975854c40ca5e47fcd
Andrew De Rango <drewderango@gmail.com>
2024-10-15 21:01:07 -0400

    create div and add info and basic styling for left sidebar

--- dcm/src/renderer/src/pages/Dashboard/Dashboard.tsx
+++ dcm/src/renderer/src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,67 @@
+import React, { useEffect, useState } from 'react'
 import LogoutButton from '../../components/LogOut/LogOut'
+import TerminateButton from '../../components/Terminate/Terminate'
 import { useUser } from '../../context/UserContext'
+import heartflowLogo from '../../assets/heartflow.png'
 import './Dashboard.css'
 
 function Dashboard(): JSX.Element {
   const { user } = useUser()
+  const [currentTime, setCurrentTime] = useState(new Date())
+  const [communicationStatus, setCommunicationStatus] = useState('Connected')
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date())
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [])
 
   return (
     <div className="dashboard-container">
-      <LogoutButton />
-      <h1>Dashboard</h1>
-      {user && <p>Welcome, {user.username}!</p>}
-      {user && <p>Your serial number is {user.serialNumber}</p>}
+      <div className="sidebar">
+        <img alt="logo" className="logo-sidebar" src={heartflowLogo} />
+        <div className="welcome-section">
+          <p className="welcome-header">Welcome</p>
+          {user && <p className="username">{user.username}</p>}
+        </div>
+        <div className="sidebar-section">
+          <p className="current-time">
+            {currentTime.toLocaleDateString('en-US', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })}
+            </p><p>{currentTime.toLocaleTimeString('en-US', {
+              hour: 'numeric',
+              minute: 'numeric',
+              second: 'numeric',
+              hour12: true,
+            })}
+          </p>
+        </div>
+        <div className="sidebar-section">
+          <p className="communication-status-header">Telemetry Status</p>
+          <p className="communication-status">{communicationStatus}</p>
+        </div>
+        <div className="bottom-sidebar-components">
+          <div className="sidebar-versions">
+            <p>HeartFlow Release: v1.0.0</p>
+            {user && <p>Serial Number: {user.serialNumber}</p>}
+          </div>
+          <div className="logout-button-container">
+            <LogoutButton />
+          </div>
+          <div className="terminate-button-container">
+            <TerminateButton />
+          </div>
+        </div>
+      </div>
+      <div className="main-content">
+        <h1>Dashboard</h1>
+        {user && <p>Welcome, {user.username}!</p>}
+        {user && <p>Your serial number is {user.serialNumber}</p>}
+      </div>
     </div>
   )
 }
