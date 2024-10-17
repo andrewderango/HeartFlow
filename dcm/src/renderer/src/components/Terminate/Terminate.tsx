fc2b25ab4cbdb3344c29778184f52333675689d7
Andrew De Rango <drewderango@gmail.com>
2024-10-17 01:33:52 -0400

    add termination disabled styling

--- dcm/src/renderer/src/components/Terminate/Terminate.tsx
+++ dcm/src/renderer/src/components/Terminate/Terminate.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { PowerOff } from 'lucide-react'
 import './Terminate.css'
 
-const TerminateButton: React.FC = () => {
+interface TerminateButtonProps {
+  onTerminate: () => void
+  disabled: boolean
+}
+
+const TerminateButton: React.FC<TerminateButtonProps> = ({ onTerminate, disabled }) => {
   const handleTermination = (): void => {
     console.log('TELEMETRY TERMINATION TO BE IMPLEMENTED HERE')
+    onTerminate()
   }
 
   return (
-    <button className="terminate-button" type="button" onClick={handleTermination}>
+    <button className="terminate-button" type="button" onClick={handleTermination} disabled={disabled}>
       <PowerOff size={24} />
       <span>Terminate Telemetry</span>
     </button>
